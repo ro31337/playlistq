84ba06a1185cf479a865f9e0780fbc4d0c2d16d4
Antonio Ochoa <adjochoa@gmail.com>
2015-11-22 21:34:03 -0800

    Cache search queries using LRU cache.

--- app/routes/search.js
+++ app/routes/search.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import LRU from 'npm:lru-cache';
 
 export default Ember.Route.extend({
   queryParams: {
@@ -7,13 +8,27 @@ export default Ember.Route.extend({
     }
   },
 
+  cache: LRU({
+    // Cache max 500 queries.
+    max: 500,
+    // Expire cached query+response after 10 minutes.
+    maxAge: 1000 * 60 * 10
+  }),
+
   model(params) {
     if (!params.query) {
       return [];
     }
 
+    let query = this.cleanQuery(params.query);
+    let cached = this.cache.get(query);
+
+    if (cached) {
+      return cached
+    }
+
     let request = window.gapi.client.youtube.search.list({
-      q: params.query,
+      q: query,
       part: 'snippet',
       type: 'video',
       maxResults: 6,
@@ -21,8 +36,16 @@ export default Ember.Route.extend({
 
     return new Ember.RSVP.Promise(resolve => {
       request.execute(response => {
+        this.cache.set(query, response);
         resolve(response);
       });
     });
+  },
+
+  // Remove extraneous characters from a query to increase the likelyhood of
+  // hitting the cache. Premature optimization? Mistakes are the best way to
+  // learn!
+  cleanQuery(query) {
+    return query.trim().toLowerCase().split(/[\s,.]+/).join(' ');
   }
 });
